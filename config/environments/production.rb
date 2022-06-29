aa8734a0de086859f99ead7c0ef76037bd8e1a97
Lovroo <sgs.lovro@gmail.com>
2022-06-29 16:59:35 +0200

    railgun fix 3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,7 @@ Rails.application.configure do
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
   config.action_mailer.default_url_options = { :host => "photoapp-rails.herokuapp.com" }
-  config.action_mailer.delivery_method = :mailgun
+  config.action_mailer.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     :port           => ENV['MAILGUN_SMTP_PORT'],
     :address        => ENV['MAILGUN_SMTP_SERVER'],
