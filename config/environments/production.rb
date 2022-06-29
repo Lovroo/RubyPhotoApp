54b712bb2098c337c78b9ea7da14a79bee191894
Lovroo <sgs.lovro@gmail.com>
2022-06-29 16:54:55 +0200

    railgun fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,8 @@ Rails.application.configure do
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'yourapp.heroku.com',
+    :api            => ENV['MAILGUN_API_KEY'],
+    :domain         => 'photoapp-rails.herokuapp.com',
     :authentication => :plain,
   }
   ActionMailer::Base.delivery_method = :smtp
